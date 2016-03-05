ab8919e9bc80a85eb124947d05d13c714b8ee8c4
Pierre Boyer <pierre.s.boyer@gmail.com>
2016-03-05 20:57:18 +0100

    Initial commit

---
+++ src/defaultRules.js
@@ -0,0 +1,111 @@
+var defaultRules =
+[
+{
+	"selector": "h2",
+	"pattern": "Syntax",
+	"translation": {
+		"fr": "Syntaxe",
+	}
+},
+{
+	"selector": "h2",
+	"pattern": "Methods",
+	"translation": {
+		"fr": "Méthodes",
+		"de": "Methoden"
+	}
+},
+{
+	"selector": "h2",
+	"pattern": "Examples",
+	"translation": {
+		"fr": "Exemples",
+		"de": "Beispiel"
+	}
+},
+{
+	"selector": "h2",
+	"pattern": "Specifications",
+	"translation": {
+		"fr": "Spécifications",
+		"de": "Spezifikationen",
+	}
+},
+{
+	"selector": "h2",
+	"pattern": "Browser compatibility",
+	"translation": {
+		"fr": "Compatibilités des navigateurs",
+		"de": "Browser Kompatibilität"
+	}
+},
+{
+	"selector": "h2",
+	"pattern": "See also",
+	"translation": {
+		"fr": "Voir aussi",
+		"de": "Siehe auch"
+	}
+},
+{
+	"selector": "h3",
+	"pattern": "Returns",
+	"translation": {
+		"fr": "Retour"
+	}
+},
+{
+	"selector": "h3",
+	"pattern": "Parameters",
+	"translation": {
+		"fr": "Paramètres",
+		"de": "Parameter"
+	}
+},
+{
+	"selector": ".compat-table th",
+	"pattern": "Feature",
+	"translation": {
+		"fr": "Fonctionnalité"
+	}
+},
+{
+	"selector": ".compat-table td",
+	"pattern": "Basic support",
+	"translation": {
+		"fr": "Support de base",
+		"de": "Basisunterstützung"
+	}
+},
+{
+	"selector": ".standard-table th",
+	"pattern": "Specification",
+	"translation": {
+		"fr": "Spécification",
+		"de": "Spezifikation"
+	}
+},
+{
+	"selector": ".standard-table th",
+	"pattern": "Status",
+	"translation": {
+		"fr": "Statut"
+	}
+},
+{
+	"selector": ".standard-table th",
+	"pattern": "Comment",
+	"translation": {
+		"fr": "Commentaire",
+		"de": "Kommentar"
+	}
+},
+{
+	"selector": ".standard-table td",
+	"pattern": "Initial definition",
+	"translation": {
+		"fr": "Définition initiale",
+		"de": "Ursprüngliche Definition"
+	}
+}
+]
